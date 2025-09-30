7a9c013f58efd4ed28e543b469060f564d52a518
Abinash-25-hue <shivanand1.july.22@gmail.com>
2025-09-30 23:49:00 +0530

    Uploaded a new problem.
    This program prints an alternating pattern of 1s and 0s starting from 1 in the 1st row, it prints 1's as per the input number then it decreases every row until the number of 1's or 0's reach zero.

---
+++ binary_triangle.cpp
@@ -0,0 +1,52 @@
+/* Print the following pattern for the given number of rows. 
+
+Input : 5
+Output : 
+11111
+0000
+111
+00
+1
+
+Time Taken : 9 mins 13.10 seconds*/
+
+#include<iostream>
+using namespace std;
+
+int binary_triangle(int rows)
+{
+    int index, flag = 1;
+    char c = '1';
+    for (int index = rows; index > 0; index--)
+    {
+        for (int jindex = 0; jindex < index; jindex++)
+        {
+            cout<< c;
+        }
+        cout<< endl;
+        //flag = (flag == 0)? 1: 0;
+        if (flag == 0)
+        {
+            flag = 1;
+            c = '1';
+        }
+        else
+        {
+            flag = 0;
+            c = '0';
+        }
+    }
+    return 0;
+
+}
+
+int main ()
+{
+    int rows;
+    cout<< "Enter the number of rows : ";
+    cin>> rows;
+    cout<< endl;
+    binary_triangle(rows);
+    cout<< endl;
+    return 0;
+}
