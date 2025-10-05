cf8c7c7d8d298d7d7fcd7f62fe1d136e79659309
Abinash-25-hue <shivanand1.july.22@gmail.com>
2025-10-05 23:37:00 +0530

    Removed unnecessary comments.

--- star_right_triangle.cpp
+++ star_right_triangle.cpp
@@ -32,18 +32,9 @@ int main ()
     int rows;
     cout<< "Enter the number of rows : ";
     cin>> rows;
-    //cout<< rows<< endl;
     
     star_printer(rows);
     int index, jindex;
-    // for (index = rows; index > 0; index--)
-    // {
-    //     for (jindex = 0; jindex < index; jindex++)
-    //     {
-    //         cout<< "* ";
-    //     }
-    //     cout<< endl;
-    // }
     return 0;
 }
 
