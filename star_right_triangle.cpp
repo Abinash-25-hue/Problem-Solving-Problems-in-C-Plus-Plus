a85865cee476cf8c65b21fae0f44525a9b51d523
Abinash-25-hue <shivanand1.july.22@gmail.com>
2025-10-05 23:35:05 +0530

    Uploaded a new problem.
    This program prints a right triangle made of asterisks ' * ' upside down by taking the base length as input.

---
+++ star_right_triangle.cpp
@@ -0,0 +1,49 @@
+/*Print the following pattern for the given N number of rows
+
+Input : 5
+Output : 
+* * * * *
+* * * * 
+* * *
+* * 
+*
+
+Time taken : 6 min and 42.51 seconds*/
+
+#include<iostream>
+using namespace std;
+
+int star_printer(int rows)
+{
+    int index, jindex;
+    for (index = rows; index > 0; index--)
+    {
+        for (jindex = 0; jindex < index; jindex++)
+        {
+            cout<< "* ";
+        }
+        cout<< endl;
+    }
+    return 0;
+}
+
+int main ()
+{
+    int rows;
+    cout<< "Enter the number of rows : ";
+    cin>> rows;
+    //cout<< rows<< endl;
+    
+    star_printer(rows);
+    int index, jindex;
+    // for (index = rows; index > 0; index--)
+    // {
+    //     for (jindex = 0; jindex < index; jindex++)
+    //     {
+    //         cout<< "* ";
+    //     }
+    //     cout<< endl;
+    // }
+    return 0;
+}
+
